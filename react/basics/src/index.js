44695b5f002f32d690cce9644ec8c105766602b3
Panth <panth.solanki@gmail.com>
2023-05-05 22:08:24 +0530

    index.js updated

--- react/basics/src/index.js
+++ react/basics/src/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+// import App from './App';
+import ErrorPage from "./error-page";
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Root from "./routes/root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    {/* <App /> */}
   </React.StrictMode>
 );
 
